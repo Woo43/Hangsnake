1e33a0be12f9db0715157b75c5a81d9cfe04963c
Woo43 <63087489+Woo43@users.noreply.github.com>
2020-04-13 20:35:43 -0700

    Add files via upload

---
+++ hangsnake.py
@@ -0,0 +1,44 @@
+print("Hangsnake!\n")
+# enter secret here
+secretword = "poyo"
+secretlist = list(secretword.upper())
+length = len(secretlist)
+mismatch = 0
+oofcount = 0
+letters_correct = 0
+dedupelist = list(dict.fromkeys(secretlist))
+dedupelen = len(dedupelist)
+winner = False
+print("\n")
+for x in secretlist:
+    print("_")
+print("\n")
+letters = []
+while oofcount < 5 and winner == False:
+    guess = input("What letter do you want to guess? ").upper()
+    if guess in letters:
+        print('You already guessed that!')
+    else:
+        if guess in secretlist:
+            letters_correct=letters_correct+1
+        letters.append(guess)
+        if len(guess) == 1:
+            for x in secretlist:
+                if x in letters:
+                    print(x)
+                    mismatch = 0
+                else:
+                    print("_")
+                    mismatch = mismatch + 1
+                    if mismatch == len(secretlist):
+                        oofcount = oofcount + 1
+                        mismatch = 0
+            if oofcount == 5:
+                print("Game over.")
+            elif letters_correct == dedupelen:
+                winner = True
+
+        else:
+            print("Stop trying to break the progam.")
+if winner == True:
+    print("You won!")
