c7f51e4b9650e570d3c580f78e7df98fd7c30583
Woo43 <63087489+Woo43@users.noreply.github.com>
2020-04-16 14:27:31 -0700

    Update hangsnake.py

--- hangsnake.py
+++ hangsnake.py
@@ -1,7 +1,9 @@
 print("Hangsnake!\n")
 # enter secret here
 secretword = input("Select a secret word. ")
-print('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
+print(
+    "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+)
 secretlist = list(secretword.upper())
 length = len(secretlist)
 mismatch = 0
@@ -17,30 +19,29 @@ print("\n")
 letters = []
 while oofcount < 5 and winner == False:
     guess = input("What letter do you want to guess? ").upper()
-    truel = length - letters_correct
     if guess in letters:
-        print('You already guessed that!')
+        print("You already guessed that!")
+    elif len(guess) != 1:
+        print("Stop trying to break the program!")
     else:
         if guess in secretlist:
-            letters_correct=letters_correct+1
-        letters.append(guess)
-        if len(guess) == 1:
-            for x in secretlist:
-                if x in letters:
-                    print(x)
-                    mismatch = 0
-                else:
-                    print("_")
-                    mismatch = mismatch + 1
-                    if mismatch == truel:
-                        oofcount = oofcount + 1
-                        mismatch = 0
-            if oofcount == 5:
-                print("Game over.")
-            elif letters_correct == dedupelen:
-                winner = True
-
+            letters_correct = letters_correct + 1
         else:
-            print("Stop trying to break the progam.")
+            oofcount = oofcount + 1
+        letters.append(guess)
+        for x in secretlist:
+            if x in letters:
+                print(x)
+#                mismatch = 0
+            else:
+                print("_")
+#                mismatch = mismatch + 1
+#                if mismatch == dedupelen:
+#                    oofcount = oofcount + 1
+#                    mismatch = 0
+        if oofcount == 5:
+            print("Game over.")
+        elif letters_correct == dedupelen:
+            winner = True
 if winner == True:
     print("You won!")
