ebbfd4b40963ab8dff985f3bdb21bcaa0a896579
Woo43 <63087489+Woo43@users.noreply.github.com>
2020-04-26 11:24:53 -0700

    Update, input validation soon

--- hangsnake.py
+++ hangsnake.py
@@ -3,7 +3,9 @@ import random
 my_list = open("wordlist.txt").readlines()
 the_list_to_end_all_lists = my_list[0].split(",")
 print("Hangsnake!\n")
-playercount = int(input("Are you playing with 1 or 2 players? "))
+playercount = 0
+while playercount == 1 or playercount == 2:
+    playercount = int(input("Are you playing with 1 or 2 players? "))
 if playercount == 2:
     secretword = input("Select a secret word. ")
     maxguesses = int(input("How manny errors do you want to give? "))
