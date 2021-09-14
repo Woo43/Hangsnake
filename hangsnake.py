6ab3808ff7ce84f41d808f85271e29190212e61b
Woo43 <63087489+Woo43@users.noreply.github.com>
2021-09-13 18:20:28 -0700

    Update hangsnake.py

--- hangsnake.py
+++ hangsnake.py
@@ -1,69 +1,79 @@
 import random
+
 print("Hangsnake!\n")
 playercount = 0
 while playercount != 1 and playercount != 2:
-    try:
-        playercount = int(input("Are you playing with 1 or 2 players? "))
-    except ValueError:
-        print("Error. Only enter 1 or 2.")
-        playercount = int(input("Are you playing with 1 or 2 players? "))
+	try:
+		playercount = int(input("Are you playing with 1 or 2 players? "))
+	except ValueError:
+		print("Error. Only enter 1 or 2.")
+		playercount = int(input("Are you playing with 1 or 2 players? "))
 if playercount == 2:
-    secretword = input("Select a secret word. ")
-    try:
-        maxguesses = int(input("How manny errors do you want to give? "))
-    except ValueError:
-        print("Number, please.")
-        maxguesses = int(input("How manny errors do you want to give? "))
-    print(
-        "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
-    )
+	allowedcharlist = open("allowedletters.txt").readlines()
+	allowedchars = allowedcharlist[0].split(",")
+	secretword = input("Select a secret word. ")
+	secondlist = list(secretword.upper())
+	if set(secondlist).issubset(set(allowedchars)) == False:
+		quit()
+	try:
+		maxguesses = int(input("How manny errors do you want to give? "))
+	except ValueError:
+		print("Number, please.")
+		maxguesses = int(input("How manny errors do you want to give? "))
+	print(
+	    "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+	)
 elif playercount == 1:
-    maxguesses = 5
-    blob = input("Smash characters, Pokemon, or words? ")
-    blobstring = blob.upper()
-    if blobstring == "WORDS":
-        my_list = open("wordlist.txt").readlines()
-    elif blobstring == "SMASH":
-        my_list = open("smashlist.txt").readlines()
-    else:
-        my_list = open("pokemonlist.txt").readlines()
-    the_list_to_end_all_lists = my_list[0].split(",")
-    secretword = random.choice(the_list_to_end_all_lists)
+	maxguesses = 5
+	blob = input("Smash characters, Pokemon, or words? ")
+	blobstring = blob.upper()
+	if blobstring == "WORDS":
+		my_list = open("wordlist.txt").readlines()
+	elif blobstring == "SMASH":
+		my_list = open("smashlist.txt").readlines()
+	else:
+		my_list = open("pokemonlist.txt").readlines()
+	the_list_to_end_all_lists = my_list[0].split(",")
+	secretword = random.choice(the_list_to_end_all_lists)
 secretlist = list(secretword.upper())
-length = len(secretlist)
+spacelist = list(" ")
 mismatch = 0
 oofcount = 0
 letters_correct = 0
 dedupelist = list(dict.fromkeys(secretlist))
-dedupelen = len(dedupelist)
+length = len(dedupelist)
 winner = False
 print("\n")
 for x in secretlist:
-    print("_", end=" ")
+	print("_", end=" ")
 print("\n")
 print("You have " + str(maxguesses) + " incorrect guesses remaining." "\n")
 letters = []
+if set(spacelist).issubset(set(secretlist)) == True:
+    letters.append(" ")
+    letters_correct += 1
 while oofcount < maxguesses and winner == False:
-    guess = input("What letter do you want to guess? ").upper()
-    if guess in letters:
-        print("You already guessed that!")
-    elif len(guess) != 1:
-        print("Stop trying to break the program!")
-    else:
-        if guess in secretlist:
-            letters_correct = letters_correct + 1
-        else:
-            oofcount = oofcount + 1
-            print("You have " + str(maxguesses-oofcount) + " incorrect guesses remaining.")
-        letters.append(guess)
-        for x in secretlist:
-            if x in letters:
-                print(x, end=" ")
-            else:
-                print("_", end=" ")
-        if oofcount == maxguesses:
-            print("Game over. The secret word was " + secretword + ".")
-        elif letters_correct == dedupelen:
-            winner = True
+	guess = input("What letter do you want to guess? ").upper()
+	if guess in letters:
+		print("You already guessed that!")
+	elif len(guess) != 1:
+		print("Stop trying to break the program!")
+	else:
+		if guess in secretlist:
+			letters_correct = letters_correct + 1
+		else:
+			oofcount = oofcount + 1
+			print("You have " + str(maxguesses - oofcount) +
+			      " incorrect guesses remaining.")
+		letters.append(guess)
+		for x in secretlist:
+			if x in letters:
+				print(x, end=" ")
+			else:
+				print("_", end=" ")
+		if oofcount == maxguesses:
+			print("Game over. The secret word was " + secretword + ".")
+		elif letters_correct == length:
+			winner = True
 if winner == True:
-    print("You won!")
+	print("You won!")
